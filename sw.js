8577ddc24c9a445550e095c3c28a28e033592efc
perpaycar2024-ai <perpaycar2024@gmail.com>
2026-04-25 18:48:00 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+﻿const CACHE_NAME = 'abuela-lista-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
